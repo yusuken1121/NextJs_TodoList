f0a3bf60b3d03c7ddc93d68f6c8c579a416775f2
yusuken1121 <yusuken1121@gmail.com>
2024-06-22 23:45:01 -0700

    ✨ Create Main Page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,7 @@ const MainLayout = ({
   return (
     <div className="flex h-screen">
       <SideMenu />
-      <main className="bg-red-300 flex-1 overflow-auto">{children}</main>
+      <main className="bg-slate-50 flex-1 overflow-auto">{children}</main>
     </div>
   );
 };
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
+import { MdAddTask } from "react-icons/md";
 
-export default function Home() {
-  return <div>Hello Next.js</div>;
+export default function MainPage() {
+  return (
+    <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
+      <header className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
+        <Link
+          href="/new"
+          className="flex items-center gap-1 font-semibold px-4 py-2 rounded-full shadow-sm text-white bg-gray-800 hover:bg-gray-700"
+        >
+          <MdAddTask className="size-5" />
+          <div>Add Task</div>
+        </Link>
+      </header>
+      <div className="mt-8 flex flex-wrap gap-4">Task Card</div>
+    </div>
+  );
 }
